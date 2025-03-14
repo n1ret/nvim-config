855da6d4c198a0ce4b734dd2005cbd9f81674793
n1ret <n1ret.lcode@gmail.com>
2025-03-14 16:47:20 +0500

    add cursor moving bind for i-mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,3 +9,10 @@ vim.keymap.set({'i', 'n'}, "<C-s>", "<cmd>:w<CR>")
 -- Clear code highlighting
 vim.keymap.set({'n'}, "<esc>", "<cmd>:let @/ = \"\"<CR>")
 
+-- Moving cursor by pressing alt and hjkl in insert mode
+keys = "hjkl"
+for i=1,#keys do
+  local c = keys:sub(i, i)
+  vim.keymap.set({'i'}, "<A-"..c..">", "<C-O>"..c)
+end
+
