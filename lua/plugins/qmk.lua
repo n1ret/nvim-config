845736b3590b447f10fcc443f34e48187c96508f
n1ret <n1ret.lcode@gmail.com>
2026-06-30 13:32:33 +0300

    QMK AutoFormatting

---
+++ lua/plugins/qmk.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "n1ret/qmk.nvim",
+    ft = "c",
+    cmd = "QMKFormat",
+    opts = {
+      name = "LAYOUT",
+      layout = {
+        "x x x x x x _ _ _ x x x x x x",
+        "x x x x x x _ _ _ x x x x x x",
+        "x x x x x x _ _ _ x x x x x x",
+        "x x x x x x x _ x x x x x x x",
+        "_ _ x x x x x _ x x x x x _ _",
+      },
+    },
+  },
+}
